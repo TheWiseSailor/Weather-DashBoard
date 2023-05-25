781ba25b5c1e0116e9d88b76cbe4051c68aa77ce
TheWiseSailor <68026214+TheWiseSailor@users.noreply.github.com>
2023-05-25 11:53:24 -0400

    went over with a bunch of CS major students while I was at UNC Charlotte, and they told me to do the  html and css in a new document, so that the original file wouldnt be messed up, and helped me fix all of the errors that were an issue for me: when eevr i type a city it wouldnt be stored under the search for a city, and the other issue was getting the icons to show up due to a 400's error. then copied everything from the document to this file. I didnt have to change much other than renaming some html and minor changes within the css and javascript here are the document links:https://docs.google.com/document/d/146U1heX_hpf8PjHnMvZr1C7YwiCCrkWGBm3R9THH1NQ/edit     https://docs.google.com/document/d/1gKXat7PLNWBWRxEvQLXxKqEpDrCFxHm8hJGl0BBvuJs/edit       https://docs.google.com/document/d/1UnAZM_wNvqTnDh5Ho9HLiQNQqY25J8RptZt-CbWW_Mg/edit        https://docs.google.com/document/d/11RpAKy51oXjJ4Xh9kCPj1W24HkibydH0pEPjPRUC-Bk/edit

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,7 @@
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-//WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-//WHEN I view future weather conditions for that city
-//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//WHEN I click on a city in the search history
-//THEN I am again presented with current and future conditions for that city
-//I want to make it to where the whole thing is centered on the screen, to which is functional with no flaws in the console or the network
-//no errors in the console log when ran
-//getting started with my api key
+// API key
 const apiKey = "1850c488fdbe3a1cd0ab670585eea465";
-//getting this all over with, starting with all the DOM elements, ive made way too many in the html
+
+// DOM elements
 const searchBar = document.getElementById("searchBar");
 const searchButton = document.getElementById("searchButton");
 const searchedCities = document.getElementById("searchedCities");
@@ -21,7 +11,7 @@ const currentIcon = document.getElementById("currentIcon");
 const currentTemperature = document.getElementById("currentTemperature");
 const currentHumidity = document.getElementById("currentHumidity");
 const currentWind = document.getElementById("currentWind");
-const CurrentUVIndex = document.getElementById("CurrentUVIndex");
+const currentUVIndex = document.getElementById("currentUVIndex");
 const day1 = document.getElementById("day1");
 const day1Icon = document.getElementById("day1Icon");
 const day1Temp = document.getElementById("day1Temp");
@@ -29,7 +19,7 @@ const day1Humidity = document.getElementById("day1Humidity");
 const day2 = document.getElementById("day2");
 const day2Icon = document.getElementById("day2Icon");
 const day2Temp = document.getElementById("day2Temp");
-const day2humidity = document.getElementById("day2humidity");
+const day2Humidity = document.getElementById("day2Humidity");
 const day3 = document.getElementById("day3");
 const day3Icon = document.getElementById("day3Icon");
 const day3Temp = document.getElementById("day3Temp");
@@ -37,38 +27,42 @@ const day3Humidity = document.getElementById("day3Humidity");
 const day4 = document.getElementById("day4");
 const day4Icon = document.getElementById("day4Icon");
 const day4Temp = document.getElementById("day4Temp");
-const day4humidity = document.getElementById("day4humidity");
+const day4Humidity = document.getElementById("day4Humidity");
 const day5 = document.getElementById("day5");
 const day5Icon = document.getElementById("day5Icon");
 const day5Temp = document.getElementById("day5Temp");
-const day5humidity = document.getElementById("day5humidity");
+const day5Humidity = document.getElementById("day5Humidity");
 
-//thisn will retrieve the searched cities from local storage
+// Retrieve the searched cities from local storage
 var searchedCitiesArr =
   JSON.parse(localStorage.getItem("searchedCities")) || [];
 
-//this will be a function to handle the search bar
+// Function to handle the search
 function handleSearch(event) {
   event.preventDefault();
 
-  const city = searchBar.ariaValueMax.trim();
+  const city = searchBar.value.trim();
   if (city !== "") {
     getWeather(city);
     searchBar.value = "";
-    //adding the city cities to the cities array
+
+    // Add the city to the searched cities array
     searchedCitiesArr.unshift(city);
-    //limiting the number of saved cities to 8 so that way it wont cover the entire left portion of the screen and cause any lagg issues if u start to go over a certain number
+
+    // Limit the number of saved cities to 8
     if (searchedCitiesArr.length > 8) {
       searchedCitiesArr.pop();
     }
-    //now to make a localstoraghe for the searched cities array in the local storage
+
+    // Save the updated searched cities array in local storage
     localStorage.setItem("searchedCities", JSON.stringify(searchedCitiesArr));
-    //making it to where it displays the searched cities
+
+    // Display the searched cities
     displaySearchedCities();
   }
 }
-//now I need to make the function to display the searched citi9es
 
+// Function to display the searched cities
 function displaySearchedCities() {
   searchedCities.innerHTML = "";
 
@@ -80,31 +74,26 @@ function displaySearchedCities() {
   }
 }
 
-function handleSearch() {
-  // Your search functionality implementation
-  searchButton.addEventListener("click", handleSearch);
-
-  searchedCities.addEventListener("click", ({ target }) => {
-    if (target.classList.contains("searchedCity")) {
-      const city = target.innerText;
-      getWeather(city);
-    }
-  });
-}
+// Add event listeners
+searchButton.addEventListener("click", handleSearch);
+searchedCities.addEventListener("click", function (e) {
+  if (e.target.classList.contains("searchedCity")) {
+    const city = e.target.innerText;
+    getWeather(city);
+  }
+});
+// Fetch weather data from API
 function getWeather(city) {
-  const apiKey = "db7e8293fc07aa24abd5218fa943315d";
-  const encodedCity = encodeURIComponent(city);
   const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
-  //fetching the current weather
+  // Fetch current weather
   fetch(currentWeatherURL)
     .then((response) => response.json())
     .then((data) => {
       // Display current weather
       displayCurrentWeather(data);
-      const currentWeatherURL =
-        "http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid={API key}";
+
       // Fetch forecast
       fetch(forecastURL)
         .then((response) => response.json())
@@ -116,15 +105,16 @@ function getWeather(city) {
     })
     .catch((error) => console.log("Error fetching current weather:", error));
 }
-//dispplaying current weather data
+// Display current weather data
 function displayCurrentWeather(data) {
   const cityName = data.name;
+  const date = new Date().toLocaleDateString("en-US");
   const iconCode = data.weather[0].icon;
   const temperature = data.main.temp;
   const humidity = data.main.humidity;
   const windSpeed = data.wind.speed;
 
-  //set current weather in the dom
+  // Set current weather values in the DOM
   currentCity.textContent = cityName;
   currentDate.textContent = date;
   currentIcon.setAttribute(
@@ -135,53 +125,107 @@ function displayCurrentWeather(data) {
   currentHumidity.textContent = humidity;
   currentWind.textContent = windSpeed.toFixed(1);
 
-  //fetching uvindex
+  // Fetch UV index
   const uvIndexURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
   fetch(uvIndexURL)
-    .then((response) => response.JSON())
+    .then((response) => response.json())
     .then((data) => {
       const uvIndex = data.value;
-      CurrentUVIndex.textContent = uvIndex;
+      currentUVIndex.textContent = uvIndex;
 
-      //now to change the background color of the uv index according to its valdue
+      // Change the background color of the UV index according to its value
       if (uvIndex < 3) {
-        CurrentUVIndex.style.backgroundColor = "green";
+        currentUVIndex.style.backgroundColor = "green";
       } else if (uvIndex < 6) {
-        CurrentUVIndex.style.backgroundColor = "yellow";
+        currentUVIndex.style.backgroundColor = "yellow";
       } else {
-        CurrentUVIndex.style.backgroundColor = "red";
+        currentUVIndex.style.backgroundColor = "red";
       }
-      CurrentUVIndex.style.color = "black";
+
+      // Adjust text color for visibility against the background color
+      currentUVIndex.style.color = "black";
     })
-    .catch((error) => console.log("Error Fetching UV index:", error));
+    .catch((error) => console.log("Error fetching UV index:", error));
 }
-//displaying the forcast data
+// Display forecast data
 function displayForecast(data) {
   const forecastList = data.list;
-  const forcastData = [];
+  const forecastData = [];
 
-  //making itto where we can separate it to 12:pm each day
+  // Filter forecast data for 12:00 PM each day
   for (let i = 0; i < forecastList.length; i++) {
-    const forecastTime = forecastList[i].dt_text.split(" ")[1];
+    const forecastTime = forecastList[i].dt_txt.split(" ")[1];
     if (forecastTime === "12:00:00") {
       forecastData.push(forecastList[i]);
     }
   }
 
-  //setting the forcast values inthe DOM
+  // Set forecast values in the DOM
   for (let i = 0; i < forecastData.length; i++) {
     const forecast = forecastData[i];
     const date = new Date(forecast.dt_txt).toLocaleDateString("en-US");
     const iconCode = forecast.weather[0].icon;
     const temperature = forecast.main.temp;
     const humidity = forecast.main.humidity;
+
+    // Set values for each forecast day
+    switch (i) {
+      case 0:
+        day1.textContent = date;
+        day1Icon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/w/${iconCode}.png`
+        );
+        day1Temp.textContent = temperature.toFixed(1);
+        day1Humidity.textContent = humidity;
+        break;
+      case 1:
+        day2.textContent = date;
+        day2Icon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/w/${iconCode}.png`
+        );
+        day2Temp.textContent = temperature.toFixed(1);
+        day2Humidity.textContent = humidity;
+        break;
+      case 2:
+        day3.textContent = date;
+        day3Icon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/w/${iconCode}.png`
+        );
+        day3Temp.textContent = temperature.toFixed(1);
+        day3Humidity.textContent = humidity;
+        break;
+      case 3:
+        day4.textContent = date;
+        day4Icon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/w/${iconCode}.png`
+        );
+        day4Temp.textContent = temperature.toFixed(1);
+        day4Humidity.textContent = humidity;
+        break;
+      case 4:
+        day5.textContent = date;
+        day5Icon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/w/${iconCode}.png`
+        );
+        day5Temp.textContent = temperature.toFixed(1);
+        day5Humidity.textContent = humidity;
+        break;
+      default:
+        break;
+    }
   }
 }
-//load the weather for a defauklt city when the page loads
-//and displaying the searched cities on the page when it loads
-//window.addEventListener("load", function () {
-//  const defaultCity = " New York";
-//  displaySearchedCities();
-//});
 
-//neeed to get this done so i can have time to work on the project mor//working on 2 projects at the same time... this will get done first because it is due tomorrow
+// Load weather for a default city on page load
+window.addEventListener("load", function () {
+  const defaultCity = "New York";
+  getWeather(defaultCity);
+
+  // Display the searched cities on page load
+  displaySearchedCities();
+});
